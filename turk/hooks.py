6fe98ddee5bec923af8ee62aedf3956ad1d20985
hamzaali15 <malikhamzaali48@gmail.com>
2021-05-19 10:37:14 +0000

    update changes

--- turk/hooks.py
+++ turk/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/turk/css/turk.css"
+app_include_css = "/assets/turk/css/turk.css"
 # app_include_js = "/assets/turk/js/turk.js"
 
 # include js, css files in header of web template
