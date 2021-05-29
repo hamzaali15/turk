889b2adb5d2cf56f516936224b7981a7b774ba5f
hamzaali15 <malikhamzaali48@gmail.com>
2021-05-29 11:51:01 +0000

    New report created named as SIH based party ledger

---
+++ turk/turk/report/sih_based_party_ledger/sih_based_party_ledger.js
@@ -0,0 +1,65 @@
+// Copyright (c) 2016, RC and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["SIH Based Party Ledger"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1,
+			"width": "60px"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "60px"
+		},
+		{
+			"fieldname":"party_type",
+			"label": __("Party Type"),
+			"fieldtype": "Link",
+			"options": "Party Type",
+			"default": "",
+			"reqd": 1,
+			on_change: function() {
+				frappe.query_report.set_filter_value('party', "");
+			}
+		},
+		{
+
+			"label": 'Party',
+			"fieldname": 'party',
+			"fieldtype": 'Dynamic Link',
+			"options": 'party_type',
+			"reqd": 1,
+			on_change: function() {
+				var party_type = frappe.query_report.get_filter_value('party_type');
+				var parties = frappe.query_report.get_filter_value('party');
+				var fieldname = erpnext.utils.get_party_name(party_type) || "name";
+				frappe.db.get_value(party_type, parties, fieldname, function(value) {
+					frappe.query_report.set_filter_value('party_name', value[fieldname]);
+				});
+			}
+		},
+		{
+			"fieldname":"party_name",
+			"label": __("Party Name"),
+			"fieldtype": "Data",
+			"read_only": 1
+		}
+	]
+};
---
+++ turk/turk/report/sih_based_party_ledger/sih_based_party_ledger.py
@@ -0,0 +1,177 @@
+# Copyright (c) 2013, RC and contributors
+# For license information, please see license.txt
+
+
+from __future__ import unicode_literals
+import frappe
+
+def execute(filters=None):
+	columns = get_columns()
+	data = get_data(filters)
+	return columns, data
+
+def get_columns():
+	columns = [
+		{
+			"fieldname": "date",
+			"fieldtype": "Date",
+			"label": "Date",
+			"width": 150
+		},
+		{
+			"label": "Voucher Type",
+			"fieldtype": "Data",
+			"fieldname": "voucher_type",
+			"width": 150
+		},
+		{
+			"label": "Voucher No.",
+			"fieldtype": "Dynamic Link",
+			"options": "voucher_type",
+			"fieldname": "voucher_no",
+			"width": 150
+		},
+		{
+			"fieldname": "item_code",
+			"fieldtype": "Link",
+			"label": "Item Code",
+			"options": "Item",
+			"width": 150
+		},
+		{
+			"fieldname": "item_name",
+			"fieldtype": "data",
+			"label": "Item Name",
+			"width": 170
+		},
+		{
+			"fieldname": "qty",
+			"fieldtype": "Float",
+			"label": "Quantity",
+			"width": 150
+		},
+		{
+			"fieldname": "rate",
+			"fieldtype": "Currency",
+			"label": "Rate",
+			"width": 150
+		},
+		{
+			"fieldname": "debit",
+			"fieldtype": "Currency",
+			"label": "Debit",
+			"width": 150
+		},
+		{
+			"fieldname": "credit",
+			"fieldtype": "Currency",
+			"label": "Credit",
+			"width": 150
+		},
+		{
+			"fieldname": "balance",
+			"fieldtype": "Currency",
+			"label": "Balance",
+			"width": 150
+		}
+	]
+	return columns
+
+def get_data(filters):
+	if filters.get('company'):
+		if filters.get('party_type') == "Customer":
+			query = """select 
+				so.posting_date as date,
+				"Sales Invoice" as voucher_type,
+				so.name as voucher_no,
+				soi.item_code,
+				soi.item_name,
+				soi.qty,
+				soi.rate,
+				soi.amount as debit,
+				0 as credit
+				from `tabSales Invoice` as so
+				left join `tabSales Invoice Item` as soi on so.name = soi.parent
+				where so.docstatus = 1 and so.company = '{0}' and so.customer = '{1}' and so.posting_date >= '{2}' and so.posting_date <= '{3}' 
+			union
+			select 
+				pe.posting_date as date,
+				"Payment Entry" as voucher_type,
+				pe.name as voucher_no,
+				'',
+				'',
+				'',
+				'',
+				0 as debit,
+				pe.paid_amount as credit
+				from `tabPayment Entry` as pe
+				where pe.docstatus = 1 and pe.party_type = 'Customer' and pe.company = '{0}' and pe.party = '{1}' and pe.posting_date >= '{2}' and pe.posting_date <= '{3}'
+				order by date
+			""".format(filters.get('company'),filters.get('party'),filters.get('from_date'),filters.get('to_date'))
+
+		if filters.get('party_type') == "Supplier":
+			query = """select 
+				po.posting_date as date,
+				"Purchase Invoice" as voucher_type,
+				po.name as voucher_no,
+				poi.item_code,
+				poi.item_name,
+				poi.qty,
+				poi.rate,
+				0 as debit,
+				poi.amount as credit
+				from `tabPurchase Invoice` as po
+				left join `tabPurchase Invoice Item` as poi on po.name = poi.parent
+				where po.docstatus = 1 and po.company = '{0}' and po.supplier = '{1}' and po.posting_date >= '{2}' and po.posting_date <= '{3}' 
+			union
+			select 
+				pe.posting_date as date,
+				"Payment Entry" as voucher_type,
+				pe.name as voucher_no,
+				'',
+				'',
+				'',
+				'',
+				pe.paid_amount as debit,
+				0 as credit
+				from `tabPayment Entry` as pe
+				where pe.docstatus = 1 and pe.party_type = 'Supplier' and pe.company = '{0}' and pe.party = '{1}' and pe.posting_date >= '{2}' and pe.posting_date <= '{3}'
+			union
+			select 
+				je.posting_date as date,
+				je.voucher_type,
+				je.name as voucher_no,
+				'',
+				'',
+				'',
+				'',
+				jea.debit,
+				jea.credit
+				from `tabJournal Entry` as je
+				left join `tabJournal Entry Account` as jea on je.name = jea.parent
+				where je.docstatus = 1 and jea.party_type = 'Supplier' and je.company = '{0}' and jea.party = '{1}' and je.posting_date >= '{2}' and je.posting_date <= '{3}'
+				order by date
+			""".format(filters.get('company'),filters.get('party'),filters.get('from_date'),filters.get('to_date'))
+		print(query)
+		result = frappe.db.sql(query,as_dict=True)
+		data = []
+		balance1 = 0
+		for row in result:
+			row.balance = row.debit - row.credit
+			balance1 += row.balance
+			row = {
+				"date": row.date,
+				"voucher_type": row.voucher_type,
+				"voucher_no": row.voucher_no,
+				"item_code": row.item_code,
+				"item_name": row.item_name,
+				"qty": row.qty,
+				"rate": row.rate,
+				"debit": row.debit,
+				"credit": row.credit,
+				"balance": balance1
+			}
+			data.append(row)
+		return data
+	else:
+		return []
